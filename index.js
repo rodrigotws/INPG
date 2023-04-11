5314b100c16557eb479f2a5ce07a8be10d1b4e3a
rodrigotws <130460713+rodrigotws@users.noreply.github.com>
2023-04-11 10:22:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
-// As quatro estações do ano variam de acordo com as datas:
-
-// Primavera: 21 setembro até 20 dezembro
-// Verão: 21 dezembro até 20 março
-// Outono: 21 março até 20 junho
-// Inverno: 21 junho até 20 setembro
-
-// Escreva uma função chamada EstacaoAno que receba como entrada um dia e um mês e retorne o nome da estação correspondente.
-
-// Formato de entrada
-
-// Dois valores inteiros, representando o dia e o mês nessa sequência
-
-// Formato de saída
-
-// Um String em letras maiúsculas
-
-// Obs: Não use acento em VERAO
-
-// Exemplos de:
-
-// Entrada
-// 1
-// 8
-
-// Saída
-// INVERNO
 const prompt = require("prompt-sync")()
 
 //assumindo que o mês tem 30 dias
 function estacaoAno(dia, mes){
-    if (mes == 9 && dia >= 21 && dia <= 30 || mes == 10 && dia >=1 && dia <= 30 || mes == 11 && dia >=1 && dia <= 30 || mes == 12 && dia <= 20 && dia <= 30){//mes <= 12 && dia <= 21 && dia >= 20){
+    if (mes == 9 && dia >= 21 && dia <= 30 || mes == 10 && dia >=1 && dia <= 30 || mes == 11 && dia >=1 && dia <= 30 || mes == 12 && dia <= 20 && dia <= 30){
         console.log("PRIMAVERA")
     }
     else if (mes == 12 && dia >= 21 && dia <= 30 || mes == 1 && dia >=1 && dia <= 30 || mes == 2 && dia >=1 && dia <= 30 || mes == 3 && dia <= 20 && dia <= 30){
