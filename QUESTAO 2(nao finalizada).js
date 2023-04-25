c9c323582b1d612ea0a666706e86fdf3f3a80057
rodrigotws <130460713+rodrigotws@users.noreply.github.com>
2023-04-25 08:48:54 -0300

    Delete QUESTAO 2(nao finalizada).js

--- QUESTAO 2(nao finalizada).js
+++
@@ -1,6 +0,0 @@
-const prompt = require ("prompt-sync")()
-
- //n = megabytes && N = meses && M_i = quantidade de megabytes
- let X = parseInt (prompt ("Qual o valor da quota?"))
- let N = parseInt (prompt("Qual o número de meses?"))
- let meses = [] //M i , mês no indice i
